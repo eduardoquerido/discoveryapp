456af5ded1d280826aa4b0a38990845fe94ccccf
Eduardo Querido <eduardo.querido@znc.com.br>
2021-03-13 15:10:08 -0300

    Adicionado inicio do projeto Discovery App
    - Adicionado a tela de login

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # discoveryapp
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
+
+Devs:
+
+    - Eduardo Querido
+    - Douglas H. Barboza
+    - João V. Cruz
---
+++ discovery_world_app/lib/discovery/login_screen.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+
+
+class LoginPage extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() => new _State();
+}
+
+class _State extends State<LoginPage> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+          backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text('Discovery World'),
+        ),
+        body: Padding(
+            padding: EdgeInsets.all(10),
+            child: ListView(
+              children: <Widget>[
+                Container(
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.all(10),
+                    child: Image.asset('assets/images/worldlogo.png')
+                    ),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: TextField(
+                    style: TextStyle(color: Colors.white),
+                    controller: nameController,
+                    decoration: InputDecoration(
+                      labelText: 'E-mail',                    
+                      labelStyle: TextStyle(
+                        color: Colors.white,
+                        fontSize: 22.0
+                      ),
+                      hintText: 'Insira seu e-mail de login',
+                      hintStyle: TextStyle(
+                        color: Colors.white70,
+                        fontSize: 14.0,
+                        
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.lightBlue, width: 5.0),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white70, width: 2.0),
+                        ),
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                  child: TextField(
+                    style: TextStyle(color: Colors.white),                    
+                    obscureText: true,
+                    controller: passwordController,
+                    decoration: InputDecoration(
+                      labelText: 'Senha',
+                      labelStyle: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0
+                      ),
+                      hintStyle: TextStyle(
+                        color: Colors.white70,
+                        fontSize: 14.0,
+                      ),
+                      hintText: 'Insira sua senha de login',                      
+                      focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.lightBlue, width: 5.0),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white70, width: 2.0),
+                        ),
+                    ),
+                  ),
+                ),
+                FlatButton(
+                  onPressed: (){
+                    //forgot password screen
+                  },
+                  textColor: Colors.white,
+                  child: Text(
+                    'Esqueceu sua senha?',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+                Container(
+                    height: 50,
+                    padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                    child: RaisedButton(
+                      textColor: Colors.blueAccent,
+                      color: Colors.white,
+                      child: Text(
+                        'Login',
+                        style: TextStyle(fontSize: 28),
+                      ),
+                      onPressed: () {
+                        print(nameController.text);
+                        print(passwordController.text);
+                      },
+                    )),
+                Container(
+                    child: Row(
+                      children: <Widget>[
+                        Text(
+                          'Não possui conta?',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.white
+                            ),
+                        ),
+                        TextButton(
+                          style: TextButton.styleFrom(
+                            primary: Colors.lightBlue.shade200,
+                          ),                          
+                          child: Text(
+                            'Inscreva-se aqui',
+                            style: TextStyle(fontSize: 15),
+                          ),
+                          onPressed: () {
+                            //signup screen
+                          },
+                        )
+                      ],
+                      mainAxisAlignment: MainAxisAlignment.center,
+                    ))
+              ],
+            )));
+  }
+}
