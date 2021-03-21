6b881531308442d1bdef23cf7373ecb22c48962f
Douglas Henrique <dev.dhbarboza@gmail.com>
2021-03-20 23:28:14 -0300

    Atualizado Escopo

--- discovery_world_app/lib/main.dart
+++ discovery_world_app/lib/main.dart
@@ -1,18 +1,14 @@
 import 'package:flutter/material.dart';
 import 'discovery/login_screen.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData(primarySwatch: Colors.blue
-      ),
-      home: LoginPage(
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: LoginPage(),
     );
   }
 }
