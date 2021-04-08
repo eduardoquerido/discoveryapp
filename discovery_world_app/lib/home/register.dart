d5f7082be6fae497b13dfc3fd994a99572108653
Douglas Henrique <dev.dhbarboza@gmail.com>
2021-04-08 12:32:39 -0300

    Integração com Firebase com erro

--- discovery_world_app/lib/home/register.dart
+++ discovery_world_app/lib/home/register.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
 
 class Registers extends StatelessWidget {
+  // Definir/ nomear rota:
+  static String tag = '/informations';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (context, index) => Container(
-          padding: EdgeInsets.all(10),
-          child: Text("Certo"),
-        ),
+      appBar: AppBar(
+        title: Text('Informações Cadastradas'),
+      ),
+      body: StreamBuilder(
+        // Buscando os dados do Firebase:
+        stream: Firestore.instance.collections('informations').snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          return ListView();
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => null,
+        tooltip: 'Adicionar novca Informação',
+        child: Icon(Icons.add),
       ),
     );
   }
--- discovery_world_app/lib/main.dart
+++ discovery_world_app/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'discovery/login_screen.dart';
 import 'home/home_screen.dart';
 import 'home/informations_items.dart';
+import 'home/register.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,8 +10,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // Remover banner debug:
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: LoginPage(),
+      // Definindo rotas:
+      initialRoute: Registers.tag,
+      // Declarando a Rota e a pagina correspondente:
+      routes: {Registers.tag: (context) => Registers()},
     );
   }
 }
